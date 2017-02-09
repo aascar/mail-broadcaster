8a0cb0662158ebbe9d0be516ea0fa08cccf06e62
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2017-02-10 00:37:52 +0530

    test folder

--- node/test/index.js
+++ node/test/index.js
@@ -1,3 +1,22 @@
 /**
  * Created by jyothi on 10/2/17.
  */
+import http from 'http';
+import assert from 'assert';
+
+import '../lib/bin/www';
+
+/**
+ * Index Test
+ */
+describe('Running Node Server', () => {
+    it('should return 200', done => {
+        http.get('http://127.0.0.1:3000', res => {
+            assert.equal(200, res.statusCode);
+            done();
+        });
+    });
+});
+
+
+
