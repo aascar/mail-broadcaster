10c23089ce824616175ff548085d3621db6fd260
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2017-02-13 23:57:44 +0530

    good to go with basic version

--- node/lib/routes/index.js
+++ node/lib/routes/index.js
@@ -10,10 +10,18 @@ router.get('/', function(req, res, next) {
 
     const broadcaster = new MailBroadcaster(conf);
 
-    //const singleRecipient = new Mail("arajajyothibabu@apxor.com", "arajajyothibabu@gmail.com", "Hai Test Mail", "This is Mail broadcaster");
+    const singleRecipient = new Mail("arajajyothibabu@apxor.com", "arajajyothibabu@gmail.com", "Hai Test Mail", "This is Mail broadcaster");
     const multiRecipient = new Mail("arajajyothibabu@apxor.com", [ "arajajyothibabu@apxor.com", "arajajyothibabu@gmail.com" ], "Hai Test Mail", "This is Mail broadcaster");
 
-    broadcaster.sendMail(multiRecipient);
+    broadcaster.sendMail(singleRecipient); //single mail
+
+    broadcaster.sendMail(multiRecipient); //multiple mails
+
+    let date = new Date(2017, 1, 13, 23, 40, 0);
+    broadcaster.scheduleMail(multiRecipient, {schedule: date}); //scheduling multiple mails
+
+    const rule = broadcaster.makeRule(30); //runs every 30 seconds
+    broadcaster.scheduleMail(multiRecipient, {schedule: rule}); //scheduling multiple mails as cron job
 
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end(response);
